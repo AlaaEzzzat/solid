607d42935a9966d55298b77bca818da22d5aa0a1
alaa <alaa.awadallah97@gmail.com>
2023-05-16 16:10:49 +0300

    OP/CP

--- src/app/features/d/data/example.ts
+++ src/app/features/d/data/example.ts
@@ -1,42 +1,103 @@
-export const Dexample = {
-  ts1: `interface AnimalInterface {
-        speak(): void;
-      }
-      
-      class Dog implements AnimalInterface {
-        speak() {
-          console.log(Dog barks.);
-        }
-      }
-      
-      class Cat implements AnimalInterface {
-        speak() {
-          console.log(Cat meows.);
-        }
-      }
-      
-      class Zoo {
-        constructor(private animals: AnimalInterface[]) {}
-      
-        addAnimal(animal: AnimalInterface) {
-          this.animals.push(animal);
-        }
-      
-        makeAllAnimalsSpeak() {
-          this.animals.forEach((animal) => animal.speak());
-        }
-      }`,
-  ts2: `class Bird implements AnimalInterface {
+import { CodeTab } from '../../s/data';
 
-        speak() {
-          console.log(Tweety tweets.);
-        }
+export const Dexample: Array<CodeTab> = [
+  {
+    name: 'Without Dependancy Inversion',
+    code: `
+    @Injectable()
+    export class HttpDataProvider {
+      constructor(private http: HttpClient) {
       }
-      
-      const myZoo = new Zoo([]);
-      myZoo.addAnimal(new Dog());
-      myZoo.addAnimal(new Cat());
-      myZoo.addAnimal(new Bird());
-      myZoo.makeAllAnimalsSpeak();
-      // Output: "Dog barks.", "Cat meows.", "Tweety tweets."`,
-};
+    
+      getUsersFromApi(): Observable<User[]> {
+        return this.http.get<User[]>('/api/users');
+      }
+    
+      getUserByIdFromApi(userId: string): Observable<User> {
+        return this.http.get<User>('/api/users/'+this.userId);
+      }
+    }
+    --------------------------------------------------------
+    @Injectable()
+    export class JsonDataProvider {
+      constructor(private http: HttpClient) {
+      }
+    
+      getUsersFromJson(): Observable<User[]> {
+        return localStorage.getItem('users');
+      }
+    
+      getUserByIdFromJson(userId: string): Observable<User> {
+        const users =localStorage.getItem('users');
+        return  this.users.find(user=>user.id === userId);
+      }
+    }
+    
+    ---------------------------------------------------
+    @Injectable()
+    export class DataService {
+      constructor(private dataProvider: HttpDataProvider ) {}
+    
+      getUsers(): Observable<User[]> {
+        return this.dataProvider.getUsersFromApi();
+      }
+    
+      getUserById(userId: string): Observable<User> {
+        return this.dataProvider.getUserByIdFromApi(userId);
+      }
+    }`,
+  },
+  {
+    name: `With Dependancy Inversion`,
+    code: `export interface DataProvider {
+  getUsers(): Observable<User[]>;
+  getUserById(userId: string): Observable<User>;
+}
+
+@Injectable()
+export class HttpDataProvider implements DataProvider {
+  constructor(private http: HttpClient) {
+    super();
+  }
+
+  getUsers(): Observable<User[]> {
+    return this.http.get<User[]>('/api/users');
+  }
+
+  getUserById(userId: string): Observable<User> {
+    return this.http.get<User>('/api/users/'+this.userId);
+  }
+}
+--------------------------------------------------------
+@Injectable()
+export class JsonDataProvider implements DataProvider {
+  constructor(private http: HttpClient) {
+    super();
+  }
+
+  getUsers(): Observable<User[]> {
+    return localStorage.getItem('users');
+  }
+
+  getUserById(userId: string): Observable<User> {
+    const users =localStorage.getItem('users');
+    return  this.users.find(user=>user.id === userId);
+  }
+}
+
+---------------------------------------------------
+@Injectable()
+export class DataService {
+  constructor(private dataProvider: HttpDataProvider ) {}
+
+  getUsers(): Observable<User[]> {
+    return this.dataProvider.getUsers();
+  }
+
+  getUserById(userId: string): Observable<User> {
+    return this.dataProvider.getUserById(userId);
+  }
+}
+`,
+  },
+];
